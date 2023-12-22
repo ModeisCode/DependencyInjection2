c49ea2d1720b01839507faa008e9a5ae62720925
ModeisCode <100340196+ModeisCode@users.noreply.github.com>
2023-12-22 16:36:18 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        //PilotPen pp = new PilotPen();
+        Pen p = new PenAdapter();
+        AssignmentWork aw = new AssignmentWork();
+        aw.setP(p);
+        aw.writeAssignment("Im bit tired to write an Assignment");
+    }
+}
